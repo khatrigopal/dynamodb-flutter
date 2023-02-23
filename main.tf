ed7651ff894e684adb56838e9711b5d71d66b1d2
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-23 20:37:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,20 @@
 module "dynamodb_tables" {
   source = "./modules/dynamodb"
-  
-    
-  tables = [
-    {
-      name          = "example-table-1"
-      billing_mode  = "PAY_PER_REQUEST"
-      hash_key      = "id"
-      range_key     =  "id"
-      attribute_name  = "id"
-      attribute_type  = "S"
+
+  tables = {
+    "table1" = {
+      name        = "table1"
+      billing_mode = "PAY_PER_REQUEST"
+      hash_key    = "id"
+      range_key   = "timestamp"
+      
     },
-    {
-      name          = "example-table-2"
-      billing_mode  = "PAY_PER_REQUEST"
-      hash_key      = "id"
-      range_key     =  "id"
-      attribute_name  = "id"
-      attribute_type  = "S"
+    "table2" = {
+      name        = "table2"
+      billing_mode = "PROVISIONED"
+      hash_key    = "user_id"
+      range_key   = "created_at"
+      
     }
-  ]
-  
-  
+  }
 }
