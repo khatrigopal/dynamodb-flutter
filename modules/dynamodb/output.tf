29dc15defa6ffbb31f4e5daeba398924147343e4
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-23 20:37:01 +0530

    Update output.tf

--- modules/dynamodb/output.tf
+++ modules/dynamodb/output.tf
@@ -1,9 +1,4 @@
-output "table_names" {
-  description = "The names of the created DynamoDB tables."
-  value       = [aws_dynamodb_table.example.*.name]
-}
-
-output "table_arns" {
-  description = "The ARNs of the created DynamoDB tables."
-  value       = [aws_dynamodb_table.example.*.arn]
+output "tables" {
+  description = "A map of the created DynamoDB tables."
+  value       = aws_dynamodb_table.tables
 }
