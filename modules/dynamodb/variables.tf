42ea8122fa9c5e3ec42e8d7bdc4575808112e177
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-23 20:36:32 +0530

    Update variables.tf

--- modules/dynamodb/variables.tf
+++ modules/dynamodb/variables.tf
@@ -1,11 +1,11 @@
 variable "tables" {
-  description = "The list of DynamoDB tables to create."
-  type = list(object({
-    name          = string
-    billing_mode  = string
-    hash_key      = string
-    range_key     = string
-    attribute_name  = string
-    attribute_type  = string
+  description = "A map of DynamoDB tables with their hash key, range key, billing mode, and tags."
+  type        = map(object({
+    name        = string
+    billing_mode = string
+    hash_key    = string
+    range_key   = string
+   
   }))
 }
+
